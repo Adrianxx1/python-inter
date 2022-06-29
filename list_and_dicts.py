baf519497296047cd8e5bc43650e064776b0d59b
Luis Adrian Amador <triaxis12q@gmail.com>
2022-06-29 11:12:33 -0500

    commit 2

--- list_and_dicts.py
+++ list_and_dicts.py
@@ -1,23 +1,24 @@
 def run():
-       my_list = [1, "Hello", True, 4.5]
-       my_dict = {"firstname": "Facundo", "lastname": "Garcia"}
 
-       super_list = [
-        {"firstname": "Facundo", "lastname": "Garcia"},
-        {"firstname": "Facundo", "lastname": "Lopez"},
-        {"firstname": "Facundo", "lastname": "Martinez"},
-        {"firstname": "Facundo", "lastname": "Ramos"},
-        {"firstname": "Facundo", "lastname": "Muñoz"},
-       ]
+    my_list = [1, "Hello", True, 4.5]
+    my_dict = {"firstname": "Facundo", "lastname": "Garcia"}
 
-       super_dict = {
-        "natural_nums": [1, 2, 3, 4, 5],
-        "integer_nums": [-1, -2, 0 1, 2],
-        "floating_nums": [1.1, 4.5, 6.43]
-       }
+    super_list = [
+    {"firstname": "Facundo", "lastname": "Garcia"},
+    {"firstname": "Facundo", "lastname": "Lopez"},
+    {"firstname": "Facundo", "lastname": "Martinez"},
+    {"firstname": "Facundo", "lastname": "Ramos"},
+    {"firstname": "Facundo", "lastname": "Muñoz"},
+     ]
 
-       for key, value in super_dict.items():
-             print(key, "-", value)
+    super_dict = {
+    "natural_nums": [1, 2, 3, 4, 5],
+    "integer_nums": [-1, -2, 0 1, 2],
+    "floating_nums": [1.1, 4.5, 6.43]
+    }
+
+    for key, value in super_dict.items():
+        print(key, "-", value)
 
 
 if __name__ == '_main_':
