23d7a1ca38e624d37af3ecd753d4900eab000947
Adrianxx1 <triaxis12q@gmail.com>
2022-06-29 23:22:27 -0500

    commit 4

--- list_and_dicts.py
+++ list_and_dicts.py
@@ -1,20 +1,19 @@
 def run():
-
     my_list = [1, "Hello", True, 4.5]
     my_dict = {"firstname": "Facundo", "lastname": "Garcia"}
 
     super_list = [
-    {"firstname": "Facundo", "lastname": "Garcia"},
-    {"firstname": "Facundo", "lastname": "Lopez"},
-    {"firstname": "Facundo", "lastname": "Martinez"},
-    {"firstname": "Facundo", "lastname": "Ramos"},
-    {"firstname": "Facundo", "lastname": "Muñoz"},
-     ]
+         {"firstname": "Facundo", "lastname": "Garcia"},
+         {"firstname": "Francisco", "lastname": "Carreon"},
+         {"firstname": "Brandon", "lastname": "Ramos"},
+         {"firstname": "Ruben", "lastname": "Martinez"},
+         {"firstname": "Lando", "lastname": "Norris"},
+    ]
 
     super_dict = {
-    "natural_nums": [1, 2, 3, 4, 5],
-    "integer_nums": [-1, -2, 0 1, 2],
-    "floating_nums": [1.1, 4.5, 6.43]
+        "natural_nums": [1, 2, 3, 4, 5],
+        "integer_nums": [-1, -2, 0, 1, 2],
+        "floating_nums": [1.1, 4.5, 6.43]
     }
 
     for key, value in super_dict.items():
